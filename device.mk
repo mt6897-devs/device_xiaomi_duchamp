3f03f8e5fccbbd2c3c0bebb082115438488c830d
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    duchamp: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # A/B
 ifeq ($(WITH_GMS),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
