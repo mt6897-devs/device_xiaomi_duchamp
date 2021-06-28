088934d6427236278b693a255b0438049dd6b2fc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-28 19:24:30 +0200

    duchamp: Be sure to build vndservicemanager
    * We need it for our blobs
    
    Change-Id: Ib757b97ee2c7d43fe606225366d729854ab987fc

--- device.mk
+++ device.mk
@@ -85,5 +85,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/duchamp/duchamp-vendor.mk)
