a08dddaa35c5af78941152336058f39579238f94
Ramji Jiyani <ramjiyani@google.com>
2022-01-27 03:10:51 +0000

    duchamp: system_dlkm: Add dynamic partiton
    Test: TH
    Bug: 200082547
    Signed-off-by: Ramji Jiyani <ramjiyani@google.com>
    Change-Id: I9779399c7b5964f6a8c9ed35140940d4ee118587

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,7 @@ BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_VENDOR_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_ODM_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
+BOARD_AVB_SYSTEM_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
 # Inherit the proprietary files
 include vendor/xiaomi/duchamp/BoardConfigVendor.mk
