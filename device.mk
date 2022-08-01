6fdce017dc95e7ae001f0e7186f5df608ca39d94
Neel Chauhan <neel@neelc.org>
2022-07-31 20:19:19 -0700

    duchamp: Disable compressed snapshots/VABc
    Virtual AB compression prevents GApps from being installed.
    
    Change-Id: I119b8e042e4cdeec27a19ab5dd4f0666cc115ae1

--- device.mk
+++ device.mk
@@ -14,8 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
-PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 PRODUCT_VIRTUAL_AB_COW_VERSION := 3
 
 PRODUCT_PACKAGES += \
