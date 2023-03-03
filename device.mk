c755316ffe7cb1c41f175955ed71a2c4693b96f3
Woomymy <woomy@woomy.be>
2023-03-03 11:48:06 +0100

    duchamp: Inherit generic_ramdisk product
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I93e080fd483d88be3a389a0ac64542ee04298f03

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
