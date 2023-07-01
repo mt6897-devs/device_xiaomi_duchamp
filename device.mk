64b403cabe7d9d08a4c97206758560a9d610fc87
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-01 17:26:41 +0700

    duchamp: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8

--- device.mk
+++ device.mk
@@ -197,6 +197,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.nxp.mifare.xml
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
