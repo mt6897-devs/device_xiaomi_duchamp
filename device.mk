9e5e5242a53022ef11949eb677899f4fcca6485b
Woomymy <woomy@woomy.be>
2023-08-27 11:36:47 +0200

    duchamp: Enable USB audio accessory support
    Change-Id: Ib5c2fee5a774ebd1153d46f856f1208fe2227bbe
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -401,6 +401,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # USB
+$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
