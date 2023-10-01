3b1068e847422a94656c880e686c08abca269cbf
SamarV-121 <samarvispute121@pm.me>
2023-10-01 23:49:17 +0100

    duchamp: Import HotwordEnrollment blobs
    Change-Id: I92d5d1ccb68e596074be6a589de6f3c3a40c850e

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.mt6897.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6897.rc
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore_V3.xml
