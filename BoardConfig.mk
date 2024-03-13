5b4d77a2fadfcb42f1ea5de41d72e8b5cd4cbde0
rio004 <riyon336@gmail.com>
2024-03-13 15:48:50 +0000

    duchamp: Define Architecture
    Change-Id: I218f22c085841e520906ea342f6fcd8029e98a30
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/xiaomi/duchamp
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
 # Inherit the proprietary files
 include vendor/xiaomi/duchamp/BoardConfigVendor.mk
