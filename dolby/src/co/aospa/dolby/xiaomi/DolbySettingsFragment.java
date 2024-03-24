b2c5c480de7e9c5762f6a562efc0adc452bd963d
Michael Bestas <mkbestas@gmail.com>
2024-03-24 17:16:21 +0530

    duchamp: dolby: Update SoundPreferenceFragment for QPR2
    The OnMainSwitchChangeListener is replaced with OnCheckedChangeListener
    
    Change-Id: I6b8cb85210eac5f0499a08a8392b3f6997542f86

--- dolby/src/co/aospa/dolby/xiaomi/DolbySettingsFragment.java
+++ dolby/src/co/aospa/dolby/xiaomi/DolbySettingsFragment.java
@@ -14,7 +14,8 @@ import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 
 import androidx.preference.ListPreference;
@@ -24,7 +25,6 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import co.aospa.dolby.xiaomi.R;
 
@@ -32,7 +32,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DolbySettingsFragment extends PreferenceFragment implements
-        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+        OnPreferenceChangeListener, OnCheckedChangeListener {
 
     private static final String TAG = "DolbySettingsFragment";
 
@@ -175,7 +175,7 @@ public class DolbySettingsFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         mDsOn = isChecked;
         mDolbyUtils.setDsOn(isChecked);
         mProfilePref.setEnabled(isChecked);
