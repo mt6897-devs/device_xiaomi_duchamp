c0a622145d07922e1ea09dc4c97a59ed270e1b4c
rio004 <riyon336@gmail.com>
2024-05-04 21:03:16 +0100

    duchamp: Inherit common MTK and Xiaomi FCMs
    Change-Id: I8fbdc3e672064202b49844cba2b915ca9bf1e597
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,9 @@ TARGET_NO_BOOTLOADER := true
 TARGET_SCREEN_DENSITY := 440
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
