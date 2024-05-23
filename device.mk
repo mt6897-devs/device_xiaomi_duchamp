e0545b0c596926750c071e7cb985b2c76cbd1e43
rio004 <riyon336@gmail.com>
2024-05-23 16:37:51 +0100

    duchamp: Drop common Xiaomi fingerprint hidl
    Also revert 0b6a4eafc64c8bc278690e351fa67718f5f5b1fd.
    
    Change-Id: I5bb9be94eab8c4a68038d4fd77e5b248302e0d6d
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -140,10 +140,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper-V1-ndk.vendor
