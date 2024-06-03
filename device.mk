c39bf76b3c2f7c7fc6e06e336a2099ee4c2a2693
rio004 <riyon336@gmail.com>
2024-06-03 21:53:11 +0530

    duchamp: Switch system partitions to EXT4 for Gapps support
    Change-Id: Ib622fa26e913e88b6e1cd5128778e4ba75816eef
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
