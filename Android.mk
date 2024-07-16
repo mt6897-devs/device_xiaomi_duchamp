4cb7951af6dfdf4d5ca37cb5a2d924e1fa55f640
rio004 <riyon336@gmail.com>
2024-07-16 22:05:28 +0100

    Revert "duchamp: Switch to DT symlinks for ODM camera libraries"
    This reverts commit 924f50ebac774db59c7549adbcc832781d629430.
    
    Change-Id: I31dd048d6cfab937c1d189238b01bfba6022593c

--- Android.mk
+++ Android.mk
@@ -9,13 +9,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),duchamp)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-ALL_SYMLINKS := $(addprefix $(TARGET_OUT_ODM)/, $(strip $(shell cat $(DEVICE_PATH)/symlink/list.txt)))
-$(ALL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf $(TARGET_BOARD_PLATFORM)/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(ALL_SYMLINKS)
-
 MDOTA_SYMLINK := $(TARGET_OUT_VENDOR)/etc/mdota
 $(MDOTA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
