c692b4befefb94df5159f6078de8483a55b98462
Pranav Vashi <neobuddy89@gmail.com>
2024-08-03 16:34:06 +0530

    dolby: Remove deprecated PlainTooltipBox
    Change-Id: I70ffff5ba30c5eeaff431e46c82eaf05d46e4cb0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- dolby/src/co/aospa/dolby/xiaomi/geq/ui/TooltipIconButton.kt
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/TooltipIconButton.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -24,8 +26,12 @@ fun TooltipIconButton(
     text: String,
     onClick: () -> Unit
 ) {
-    PlainTooltipBox(
-        tooltip = { Text(text) }
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        tooltip = {
+            Text(text)
+        },
+        state = rememberTooltipState()
     ) {
         IconButton(
             onClick = onClick
