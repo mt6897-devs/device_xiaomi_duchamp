7b5d21e568fc3b56b499c11c2a8e94c09fdd5ee7
rio004 <riyon336@gmail.com>
2024-08-16 20:43:00 +0100

    duchamp: Build necessary camera library
    Change-Id: Ia07564163d9127343f545a5221690d3697239993
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider@2.6.vendor \
     libcamera2ndk_vendor \
+    libjpeg.vendor \
     libutils-v34
 
 # ConsumerIr
