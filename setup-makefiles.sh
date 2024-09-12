ec94b55b0717bf79eb2eaeb5e158e5b4520c928d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-12 23:55:37 +0100

    duchamp: Append `-vendor` to conflicting vendor libs
    Change-Id: I96d4bb7fd318c67abe84a75bd683c5c8c743a6f2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,6 +25,33 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        libmialgo_aio_seg | \
+            libmialgo_utils | \
+            vendor.mediatek.hardware.mtkpower-V1-ndk | \
+            vendor.mediatek.hardware.mtkpower@1.0 | \
+            vendor.mediatek.hardware.mtkpower@1.1 | \
+            vendor.mediatek.hardware.mtkpower@1.2 | \
+            vendor.mediatek.hardware.videotelephony-V1-ndk)
+            echo "$1-vendor"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
