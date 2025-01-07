924af1b406f9d5b162e06e613367453799fcd415
Arian <arian.kulmer@web.de>
2025-01-07 17:42:19 +0100

    duchamp: udfps: Update fod_finger_state node based on actual udfps state
    Change-Id: I9f468457acfe373b9fdab7df7a9065905629263c

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -16,6 +16,7 @@
 #include <thread>
 
 #include <drm/mi_disp.h>
+#include <linux/xiaomi_touch.h>
 
 #include "UdfpsHandler.h"
 
@@ -28,6 +29,7 @@
 #define PARAM_FOD_RELEASED 0
 
 #define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 
 using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
@@ -76,6 +78,7 @@ class XiaomiDuchampUdfpsHandler : public UdfpsHandler {
     void init(fingerprint_device_t* device) {
         mDevice = device;
         disp_fd_ = android::base::unique_fd(open(DISP_FEATURE_PATH, O_RDWR));
+        touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
 
         // Thread to listen for fod ui changes
         std::thread([this]() {
@@ -135,6 +138,13 @@ class XiaomiDuchampUdfpsHandler : public UdfpsHandler {
 
         mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_PRESSED);
 
+        // Update fod_finger_state node in case hwmodule polls it
+        struct touch_mode_request touchRequest = {
+                .mode = TOUCH_MODE_FOD_FINGER_STATE,
+                .value = 1,
+        };
+        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &touchRequest);
+
         // Request HBM
         struct disp_local_hbm_req displayLhbmRequest = {
                 .base = displayBasePrimary,
@@ -155,6 +165,12 @@ class XiaomiDuchampUdfpsHandler : public UdfpsHandler {
         };
         ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
 
+        // Update fod_finger_state node in case hwmodule polls it
+        struct touch_mode_request touchRequest = {
+                .mode = TOUCH_MODE_FOD_FINGER_STATE,
+                .value = 0,
+        };
+        ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &touchRequest);
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
@@ -185,6 +201,7 @@ class XiaomiDuchampUdfpsHandler : public UdfpsHandler {
   private:
     fingerprint_device_t* mDevice;
     android::base::unique_fd disp_fd_;
+    android::base::unique_fd touch_fd_;
 };
 
 static UdfpsHandler* create() {
