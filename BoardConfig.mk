c90503b0e3c8f40af4530ebcef8699349f74520c
rio004 <riyon336@gmail.com>
2026-06-30 21:52:08 +0100

    duchamp: Define architecture flags
    Change-Id: I838167bc09a989bc3770d1b26ad59103cc73aed1
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv9-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a55
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := duchamp
 TARGET_NO_BOOTLOADER := true
